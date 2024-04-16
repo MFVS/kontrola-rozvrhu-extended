4dea44dc4b1a3c9a2054d82da433c5829877095a
Berankovan <161521521+Berankovan@users.noreply.github.com>
2024-04-16 13:17:01 +0200

    Přidala jsem checkboxy

--- app_ps.py
+++ app_ps.py
@@ -5,17 +5,20 @@ st.subheader("Vyplňte následující dotazník:")
 
 main_options = st.selectbox("Hledat chyby podle:",["Fakulta","Katedra","Studijní program","Učitel"])
 if main_options == "Fakulta":
-    st.selectbox("Zvolte fakultu:",["PřF","PF","FSE","FSI","FF","FZS","FŽP","FUD"])
+    st.multiselect("Zvolte fakultu:",["PřF","PF","FSE","FSI","FF","FZS","FŽP","FUD"])
 if main_options == "Katedra":
-    st.selectbox("Zvolte katedru:",["KMA","KI","zbytek doplníme"])
+    st.multiselect("Zvolte katedru:",["KMA","KI","zbytek doplníme"])
 if main_options == "Studijní program":
-    st.selectbox("Zvolte studijní program:",["MFVS","Aplikovaná informatika","Ekonomika a management","Chemie a toxikologie","Geografie","a tak dále"])
+    st.multiselect("Zvolte studijní program:",["MFVS","Aplikovaná informatika","Ekonomika a management","Chemie a toxikologie","Geografie","a tak dále"])
 if main_options == "Učitel":
-    st.selectbox("Zvolte učitele:",["učitel1","učitel2","učitel3","a tak dále"])
+    st.multiselect("Zvolte učitele:",["učitel1","učitel2","učitel3","a tak dále"])
 
 st.selectbox("Zvolte akademický rok:",["2023/2024","2022/2023","2021/2022","2020/2021","a tak dále"])
 
-st.multiselect("Zvolte, které chyby chcete zobrazit:",["chyba 1","chyba 2","chyba 3", "chyba 4", "chyba 5"])
+cols = st.columns(3) 
+for i in range(3): 
+    with cols[i]: 
+        num = st.checkbox(f'chyba {i}', value = True) 
 
 st.selectbox("Zvolte jazyk:",["čeština","angličtina"])
 
