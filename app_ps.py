b9072f81c02ea7938b34867ea6772d7929fa3e60
Pavel Klas <94009466+KLASlK@users.noreply.github.com>
2024-04-18 18:23:48 +0200

    Přidání sloupečků k selectboxům

--- app_ps.py
+++ app_ps.py
@@ -12,24 +12,30 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.title("Služba na hledání chyb v IS STAG")
 st.subheader("Vyplňte následující dotazník:")
 
-main_options = st.selectbox("Hledat chyby podle:",["Fakulta","Katedra","Studijní program","Učitel"])
-if main_options == "Fakulta":
-    st.multiselect("Zvolte fakultu:",["PřF","PF","FSE","FSI","FF","FZS","FŽP","FUD"])
-if main_options == "Katedra":
-    st.multiselect("Zvolte katedru:",["KMA","KI","zbytek doplníme"])
-if main_options == "Studijní program":
-    st.multiselect("Zvolte studijní program:",["MFVS","Aplikovaná informatika","Ekonomika a management","Chemie a toxikologie","Geografie","a tak dále"])
-if main_options == "Učitel":
-    st.multiselect("Zvolte učitele:",["učitel1","učitel2","učitel3","a tak dále"])
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    main_options = st.selectbox("Hledat chyby podle:",["Fakulta","Katedra","Studijní program","Učitel"])
+
+with col2:
+    if main_options == "Fakulta":
+        st.multiselect("Zvolte fakultu:",["PřF","PF","FSE","FSI","FF","FZS","FŽP","FUD"])
+    if main_options == "Katedra":
+        st.multiselect("Zvolte katedru:",["KMA","KI","zbytek doplníme"])
+    if main_options == "Studijní program":
+        st.multiselect("Zvolte studijní program:",["MFVS","Aplikovaná informatika","Ekonomika a management","Chemie a toxikologie","Geografie","a tak dále"])
+    if main_options == "Učitel":
+        st.multiselect("Zvolte učitele:",["učitel1","učitel2","učitel3","a tak dále"])
 
 # Fakulta = st.selectbox("Zvolte fakultu:",["By default","","","","",""])
 # if Fakulta == "By default":
     
+with col3:
+    st.selectbox("Zvolte akademický rok:",["2023/2024","2022/2023","2021/2022","2020/2021","a tak dále"])
 
-st.selectbox("Zvolte akademický rok:",["2023/2024","2022/2023","2021/2022","2020/2021","a tak dále"])
-
-cols = st.columns(3) 
-for i in range(3): 
+st.subheader("Filtrování typů chyb")
+cols = st.columns(7) 
+for i in range(7): 
     with cols[i]: 
         num = st.checkbox(f'chyba {i+1}', value = True) 
 
