e70517468e2188e9432ecb640a583f9b7734f9d6
Hexex36 <matej@sloupovi.info>
2024-09-13 13:13:16 +0200

    Added ansi conversion script

--- pages/1_Výpis_výsledků.py
+++ pages/1_Výpis_výsledků.py
@@ -14,6 +14,22 @@ chyby_translator = {
     "Seminářicí mimo sylabus":"seminare_bez_seminaricich",
 }
 
+# chyby_translator = {
+#     "Bez garanta":"bez_garanta",
+#     "Bez přednášejících":"chybi_prednasejici",
+#     "Bez cvičích":"chybi_cvicici",
+#     "Bez seminařicích":"chybi_seminarici",
+#     "Vice garantu":"vice_garantu",
+#     "Garant nepřednáší":"garant_neprednasi",
+#     "Garant neučí":"predmety_kde_garant_neuci",
+#     "Přednášející bez přednášek":"prednasejici_bez_prednasek",
+#     "Cvičící bez cvičení":"sample_file",
+#     "Seminařící bez seminářů":"sample_file",
+#     "Přednášející mimo sylabus":"sample_file",
+#     "Cvičicí mimo sylabus":"sample_file",
+#     "Seminářicí mimo sylabus":"sample_file",
+# }
+
 import polars as pl
 import streamlit as st
 chyby = [name for name in st.session_state["wishes"].keys() if st.session_state["wishes"][name] == True]
@@ -30,11 +46,14 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.header("Výpis chyb")
 
+#TODO: Tlačítko na stažení všech souborů
+
 tab_folder = st.tabs(chyby)
 
 for tab_index,tab in enumerate(tab_folder):
-    with open("results_csv/"+chyby_translator[chyby[tab_index]]+"_"+st.session_state["stagRoleName"]+".csv") as file:
-        tab.download_button("Stáhnout csv", file, file_name=chyby[tab_index]+".csv")
+    pre_ansi = ""
+    with open("results_csv/"+chyby_translator[chyby[tab_index]]+"_"+st.session_state["stagRoleName"]+".csv", "rb") as file:
+        tab.download_button("Stáhnout csv", file, file_name=chyby[tab_index]+".csv") #TODO: Překonvertovat soubory z utf-8 na ANSI. Jinak to v excelu vyplivne gibberish.
     tab.dataframe(pl.read_csv("results_csv/"+chyby_translator[chyby[tab_index]]+"_"+st.session_state["stagRoleName"]+".csv"))
 
 
